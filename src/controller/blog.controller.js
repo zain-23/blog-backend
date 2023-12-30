b6d5519e27df3451bfe056c8c3b9e6d5f638df03
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2023-12-30 18:47:22 +0500

    create a delete controller and update,get published and get unpublished data

--- src/controller/blog.controller.js
+++ src/controller/blog.controller.js
@@ -1,9 +1,10 @@
 import { BLOG } from "../model/blog.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 
-const addBlog = async (req, res) => {
+const addBlog = asyncHandler(async (req, res) => {
   const { author, category, title } = req.body;
 
   if ([author, category, title].some((fields) => fields === "" || undefined)) {
@@ -18,7 +19,7 @@ const addBlog = async (req, res) => {
   if (req.file) {
     thumbnailLocalPath = req.file?.path;
   }
-
+  console.log(thumbnailLocalPath);
   if (!thumbnailLocalPath) {
     throw new ApiError(401, "Invalid thumbnail image");
   }
@@ -46,6 +47,120 @@ const addBlog = async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(201, savedBlog, "blog created successfullt"));
-};
+});
 
-export { addBlog };
+const updateBlog = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+  const { id } = req.params;
+  console.log(req.body, id);
+
+  if (!content) return null;
+
+  const updatedBlog = await BLOG.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updatedBlog) {
+    throw new ApiError(500, "Can't updated the blog try again later");
+  }
+
+  return res.status(200, updatedBlog, "Blog updated successfully");
+});
+
+const deleteBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  console.log("id", id);
+  if (!id) {
+    throw new ApiError(401, "invalid blog id");
+  }
+
+  await BLOG.findByIdAndDelete(id);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, "Blog deleted successfully"));
+});
+
+const getAllBlog = asyncHandler(async (req, res) => {
+  const allBlogs = await BLOG.find();
+
+  if (allBlogs.length === 0) {
+    throw new ApiError(401, "no blogs");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, allBlogs, "Get Blogs successfully"));
+});
+
+const getBlogByCotegory = asyncHandler(async (req, res) => {
+  const { category } = req.params;
+
+  if (!category) {
+    throw new ApiError(401, "cannot find blogs");
+  }
+
+  const blogs = await BLOG.aggregate([
+    {
+      $match: {
+        category,
+      },
+    },
+  ]);
+
+  console.log("blogs", blogs);
+});
+
+const getPublishBlog = asyncHandler(async (req, res) => {
+  const publishedBlog = await BLOG.aggregate([
+    {
+      $match: {
+        isPublished: true,
+      },
+    },
+  ]);
+  if (publishedBlog.length === 0) {
+    throw new ApiError(401, "No publishedBlog data");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(201, publishedBlog, "get publishedBlog data successfully")
+    );
+});
+
+const getDraftBlog = asyncHandler(async (req, res) => {
+  const draftBlog = await BLOG.aggregate([
+    {
+      $match: {
+        isPublished: false,
+      },
+    },
+  ]);
+  if (draftBlog.length === 0) {
+    throw new ApiError(401, "No draft data");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, draftBlog, "get draft data successfully"));
+});
+
+export {
+  addBlog,
+  updateBlog,
+  deleteBlog,
+  getAllBlog,
+  getBlogByCotegory,
+  getPublishBlog,
+  getDraftBlog,
+};
--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { addBlog } from "../controller/blog.controller.js";
+import {
+  addBlog,
+  deleteBlog,
+  getAllBlog,
+  getBlogByCotegory,
+  getDraftBlog,
+  updateBlog,
+} from "../controller/blog.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 
 const router = Router();
 
 router.route("/add-blog").post(upload.single("thumbnail"), addBlog);
+router.route("/delete-blog/:id").post(deleteBlog);
+router.route("/update-blog/:id").patch(updateBlog);
+router.route("/get-blogs").get(getAllBlog);
+router.route("/get-draftblogs").get(getDraftBlog);
+router.route("/get-blogs/:category").get(getBlogByCotegory);
 
 export default router;
