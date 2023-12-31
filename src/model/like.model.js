536c3aca2e4d322f30a518bd0357b361abe627bd
Syed zain ali shah <137421662+zain-23@users.noreply.github.com>
2024-01-01 00:19:59 +0500

    create a like schema

---
+++ src/model/like.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    avatar: {
+      type: String,
+      required: [true, "avatar is required"],
+    },
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    blog: {
+      type: Schema.Types.ObjectId,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const LIKE = mongoose.model("Like", likeSchema);
