558209fb5f866e1ce1cc0ea43afc13e96babcfb6
Syed zain ali shah <zaingit780@gmail.com>
2024-01-08 12:59:07 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,10 +7,11 @@ import cors from "cors";
 const app = express();
 
 app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CORS_ORIGIN,
-  })
+  // cors({
+  //   credentials: true,
+  //   origin: process.env.CORS_ORIGIN,
+  // })
+  cors()
 );
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
